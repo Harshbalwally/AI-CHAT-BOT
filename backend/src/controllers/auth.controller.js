67246feadaeb034464274e661c4ed0b652dd4873
Harshbalwally <harshbalwally907@gmail.com>
2025-01-28 22:05:38 +0530

    naya code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import bcrypt from "bcryptjs";
 export const signup = async ( req, res) => {
     const{fullName,email,password} = req.body
     try {
-        if (!fullname || !email || !password){
+        if (!fullName || !email || !password){
             return res.status(400).json({ messages : "all fields are required"});
            }
        if (password.length < 6){
@@ -44,8 +44,33 @@ export const signup = async ( req, res) => {
     }
 };
 
-export const login = ( req, res) => {
-    res.send("login route");
+export const login = async( req, res) => {
+    const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+
 };
 
 export const logout = ( req, res) => {
