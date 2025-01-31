910c0eb8fd724e46e979ace5f44cfa08ad8525b1
Harshbalwally <harshbalwally907@gmail.com>
2025-01-31 23:36:09 +0530

    aaj jyada nahi kia kal karunga me

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { protectRoute } from "../middleware/auth.middleware";
-import { getMessages, getUserForSidebar } from "../controllers/message.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router =express.Router();
+const router = express.Router();
 
+router.get("/users", protectRoute, getUserForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-router.get("/users",protectRoute,getUserForSidebar)
-router.get("/:id",protectRoute,getMessages)
+router.post("/send/:id", protectRoute,sendMessage);
 
 export default router;
